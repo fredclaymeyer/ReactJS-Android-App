827c34e376808200e42f8ab928890e26b454de6c
Clara Baron <cbaronhy@Claras-MacBook-Pro.local>
2017-01-25 15:02:51 -0600

    Lots of progress

--- index.android.js
+++ index.android.js
@@ -10,28 +10,87 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
+  Dimensions,
+  Animated,
   TouchableOpacity
 } from 'react-native';
 
+const backupForNoFetched = 'Have a good day! :)';
+const screenTransitionPauseInDeciseconds = 10;
+
+getFullWidth = () => {
+  return Dimensions.get('window').width;
+}
+
+getFullHeight = () => {
+  return Dimensions.get('window').height;
+}
+
 export default class DeepBreaths extends Component {
+
   constructor() {
     super();
     this.state = {
       userHasStarted: false,
-      deciseconds: -10,
-      breathDuration: 140,
-      breathCount: 3
+      deciseconds: screenTransitionPauseInDeciseconds * -1,
+      breathDuration: 14,
+      breathCount: 3,
+      requestSource: 'https://wpshout.com/wp-json/threedeepbreaths/v1/happyimages',
+      fetchedURL: false,
     };
   }
 
   componentDidMount() {
-    // setInterval(() => {
-    //   this.setState({ deciseconds: this.state.deciseconds + 1 });
-    // }, 100);
+    fetch(this.state.requestSource)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        // console.error(responseJson[0]);
+        let url = responseJson[0];
+        if( typeof(url) !== 'undefined' ) {        
+          this.setState({
+            fetchedURL: url,
+          });
+        }
+      }).catch((error) => {
+        // alert("No internet?");
+      });
   }
 
+
+
+    // let me = fetch('https://facebook.github.io/react-native/movies.json');
+
+    // me = me.then( 
+    //     function() {
+    //       // alert("DIE");
+    //       return "stfu";
+    //      // return "DIE";          
+    //     }
+    //     // (jsonResponse) => {
+    //     //   return "LOLSTFU"
+    //     // }
+    //   ).catch(
+    //     (error) => {
+    //       return "ERRR";
+    //     }
+    //   );
+
+    //   return me;
+
+      // .then( (jsonResponse) => jsonResponse.json() )
+      // .then( (parsedJson) => "HELP"
+      // ).catch((fetchError) => {
+      //   console.error(fetchError);
+      // }); 
+  // }
+
   componentWillUnmount() {
-    this.serverRequest.abort();
+    // this.state.serverRequest.abort();
+  }
+
+  getArticleTitle = () => {
+    'Hi';
   }
 
   startAfterButtonPress() {
@@ -39,18 +98,16 @@ export default class DeepBreaths extends Component {
       userHasStarted: true
     });
 
-    setTimeout(function() {
-      let runTimer = setInterval(function() {
-        this.setState({
-          deciseconds: this.state.deciseconds + 1
-        }, 
-        function() {
-          if( this.isDoneBreathing() ) {
-            clearInterval(runTimer);
-          }
-        });
-      }.bind(this),100);
-    }.bind(this),1000);
+    let runTimer = setInterval(function() {
+      this.setState({
+        deciseconds: this.state.deciseconds + 1
+      }, 
+      function() {
+        if( this.isDoneBreathing() ) {
+          clearInterval(runTimer);
+        }
+      });
+    }.bind(this),100);
   }
 
   getDecisecondsModulus() {
@@ -108,45 +165,56 @@ export default class DeepBreaths extends Component {
   }
 
   breathingBackground() {
-    let opacity = .25 + .5 * this.returnBreathRhythmFraction();
+    let size = 100 * this.returnBreathRhythmFraction();
     return {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'rgba(255,255,255,' + opacity + ')',
+      position: 'absolute',
+      width: size,
+      height: size,
+      borderRadius: size/2,
+      backgroundColor: 'red',
     }
   }
 
   render() {
     return (
-      <View style={ styles.container }>
-        <View style={ this.breathingBackground() }>
-          <Button hidden={ this.state.userHasStarted } clickHandler={ this.startAfterButtonPress.bind(this) } />
-          <Count hidden={ this.isNotBreathing() } breaths={ this.calculateBreathCount() } />
-          <Message hidden={ ! this.isDoneBreathing() } content="Don't you feel better? :)" />
-        </View>
+      <View style={ styles.outercontainer }>
+        <View style={ this.breathingBackground() } />
+        <Button hidden={ this.state.userHasStarted } clickHandler={ this.startAfterButtonPress.bind(this) } />
+        <Count hidden={ this.isNotBreathing() } breaths={ this.calculateBreathCount() } />
+        <Message hidden={ ! this.isDoneBreathing() } content={ this.state.fetchedURL } />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  outercontainer: {
     flex: 1,
-    justifyContent: 'space-between',
-    alignItems: 'stretch',
-    backgroundColor: '#22aa55',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eeeeee',
+  },
+  container: {
+    width: getFullWidth(),
+    height: getFullHeight(),
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   count: {
     fontSize: 100,
   },
   message: {
-    fontSize: 20,
+    fontSize: 50,
     textAlign: 'center',
-    margin: 10,
+    padding: 0,
+    margin: 50,
+    color: '#ffffff',
+  },
+  imageholder: {
+    width: 100,
+    height: 100,
   },
   button: {
-    
     backgroundColor: 'rgba(255,255,255,.5)',
     paddingTop: 15,
     paddingBottom: 15,
@@ -178,14 +246,67 @@ class Count extends Component {
 
 class Message extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+       fadeValue: new Animated.Value(0),
+    }
+  }
+
+
+  imageLayout() {
+    return {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      opacity: .7,
+    }
+  } 
+
+  animLayout() {
+    this.state.fadeValue.setValue(0);     // Start small
+    Animated.timing(                          // Base: spring, decay, timing
+      this.state.fadeValue,                 // Animate `fadeValue`
+      {
+        toValue: 1,                         // Animate to larger size
+        duration: 1000,
+      }
+    ).start();                                // Start the animation
+
+    return {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      opacity: this.state.fadeValue,  // Map `fadeValue` to `opacity`
+    }
+  }
+
   render() {
+    let imgSrc = this.props.content;
+
     if( this.props.hidden === true ) {
       return ( <Text /> );
     }
+
+    if( imgSrc === false ) {
+      return (
+        <Text style={ styles.message }>
+          { backupForNoFetched }
+        </Text>
+      );
+    }
+
     return (
-      <Text style={ styles.message }>
-        { this.props.content }
-      </Text>
+        <View style={ styles.container }>
+          <Image style={ this.imageLayout() } source={{uri: imgSrc }} />
+          <Animated.View style={ this.animLayout() }>
+            <Text style={ styles.message }>
+              { backupForNoFetched }
+            </Text>
+          </Animated.View>
+        </View>
     );
   }
 }
@@ -199,7 +320,7 @@ class Button extends Component {
       <TouchableOpacity style={styles.button} onPress={this.props.clickHandler
       }>
         <View>
-          <Text style={styles.buttontext} onPress={this.props.clickHandler} >Ready</Text>
+          <Text style={styles.buttontext} onPress={this.props.clickHandler} >Ready?</Text>
         </View>
       </TouchableOpacity>
     );
